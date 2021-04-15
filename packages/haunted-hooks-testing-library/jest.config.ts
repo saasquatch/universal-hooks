88c67506df9df84b3759ad0d02371c5eed6e4226
Malcolm Seyd <malcolm.seyd@referralsaasquatch.com>
2021-04-15 11:19:45 -0700

    use ts-jest instead of esbuild-jest where possible
    esbuild-jest screws up which line the test failed on and means you
    have to step through compiled js when debugging. ts-jest is preferable

--- packages/haunted-hooks-testing-library/jest.config.ts
+++ packages/haunted-hooks-testing-library/jest.config.ts
@@ -5,7 +5,8 @@ const esModules = ["haunted", "lit-html"].join("|");
 const config: Config.InitialOptions = {
   testMatch: ["**/test/main.ts"],
   transform: {
-    "^.+\\.(ts|tsx|js|jsx)$": "esbuild-jest",
+    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(js|jsx)$": "esbuild-jest",
   },
   transformIgnorePatterns: [`node_modules/(?!(${esModules})/)`]
 };
